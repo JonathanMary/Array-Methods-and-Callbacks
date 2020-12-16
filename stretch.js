7b8a150b06d6e1ff5bf6da48b44ac11c7aa579b0
JonathanMary <jon85ma@gmail.com>
2020-12-16 02:26:34 +0100

    3/4 stretch Done

--- stretch.js
+++ stretch.js
@@ -25,6 +25,35 @@ function countryCode(initials){
     return result.length;
     //return total number of world cup appearances
 }
-
 //console.log(countryCode("MEX"));  //returns number of appearances (years) of MEXico in world cup.
 
+
+/* Stretch 2:
+Account for ties in your 'finals' data set
+!! Did it in the index.js, need to uncomment and the good worldcup winners will show.*/
+
+/* Stretch 3:
+Create a function that takes country initials as a parameter 
+and determines how many goals that country has scored in World Cup games since 1930. */
+
+function countryTotGoals(initials){
+    //total goals counter.
+    let total = 0;
+
+    function findCode(value){
+        let home = value["Home Team Initials"];
+        let away = value["Away Team Initials"];
+
+        //check for country initials, then add it to total;
+        if (home === initials){
+            total += value["Home Team Goals"];
+        }else if (away === initials){
+            total += value["Away Team Goals"];
+        }
+    }
+    const allGames = fifaData.forEach(findCode);
+    
+    return total;
+}
+
+//console.log(countryTotGoals("FRA")); //returns total goals scored by FRA over all world cup games played.
